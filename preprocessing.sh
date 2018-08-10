e2a76348a105498b2e498ecd158ad4be1d431599
Wanwen Zeng <241496@qq.com>
2018-08-09 19:52:10 -0700

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,5 +1,6 @@
 #Please put all the single cell RNA-seq bed files into ./Bed/ folder
 
+#Get PeakO.txt file
 cat ./Bed/* |sort -k1,1 -k2,2n> merge.bed
 ls ./Bed/ > ATAC_SampleName
 
@@ -15,11 +16,7 @@ bedtools intersect -a region.bed -b ./Bed/${line} -wa -c -sorted|cut -f 4 >a
 paste -d '\t' region_read.bed a>a1
 cat a1 > PeakO.txt
 done
-cat region_read.bed |cut -f 1 >region_name.txt
-cat RE_v2.bed|tr '\t' '_'>RE_read.bed
-cat ATAC_SampleName|while read line
-do
-bedtools intersect -a RE_v2.bed -b ./Bed/${line} -wa -c|cut -f 4 >a
-paste -d '\t' RE_read.bed a>a1
-cat a1 > REO.txt
-done
+#Get peak_gene_100k_corr.bed file
+genome=mm9
+bedtools intersect -a common_data/Promoter_100k_${genome}.bed  -b Peak.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{print $5,$6,$7,$4,$3-100000-$6}'|sed 's/-//g'|sort -k1,1 -k2,2n >peak_gene_100k.bed
+bedtools intersect -a peak_gene_100k.bed -b common_data/RE_gene_corr_${genome}.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{if ($4==$9) print $1,$2,$3,$4,$5,$10}'|sed 's/\t/\_/1'|sed 's/\t/\_/1'>peak_gene_100k_corr.bed
