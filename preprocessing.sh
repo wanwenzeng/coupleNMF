450b1086a4445e0fb0fb38c26e4542d219a29dca
Wanwen Zeng <241496@qq.com>
2018-08-10 13:37:35 -0700

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -24,5 +24,5 @@ done
 cat region_read.bed |cut -f 1 >PeakName.txt
 
 #Get peak_gene_100k_corr.bed file
-bedtools intersect -a common_data/Promoter_100k_${genome}.bed  -b Peak.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{print $5,$6,$7,$4,$3-100000-$6}'|sed 's/-//g'|sort -k1,1 -k2,2n >peak_gene_100k.bed
+bedtools intersect -a common_data/Promoter_100k_${genome}.bed  -b region.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{print $5,$6,$7,$4,$3-100000-$6}'|sed 's/-//g'|sort -k1,1 -k2,2n >peak_gene_100k.bed
 bedtools intersect -a peak_gene_100k.bed -b common_data/RE_gene_corr_${genome}.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{if ($4==$9) print $1,$2,$3,$4,$5,$10}'|sed 's/\t/\_/1'|sed 's/\t/\_/1'>peak_gene_100k_corr.bed
