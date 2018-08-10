7093c745c27f605a26f6e98fb4cbc715e39971a1
Wanwen Zeng <241496@qq.com>
2018-08-10 11:04:04 -0700

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,5 +1,6 @@
+#We now support mm9 or hg19 only, change the genome to your interested genome
+genome=mm9  
 #Please put all the single cell RNA-seq bed files into ./Bed/ folder
-
 #Get PeakO.txt file
 cat ./Bed/* |sort -k1,1 -k2,2n> merge.bed
 ls ./Bed/ > ATAC_SampleName
@@ -16,7 +17,7 @@ bedtools intersect -a region.bed -b ./Bed/${line} -wa -c -sorted|cut -f 4 >a
 paste -d '\t' region_read.bed a>a1
 cat a1 > PeakO.txt
 done
+
 #Get peak_gene_100k_corr.bed file
-genome=mm9
 bedtools intersect -a common_data/Promoter_100k_${genome}.bed  -b Peak.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{print $5,$6,$7,$4,$3-100000-$6}'|sed 's/-//g'|sort -k1,1 -k2,2n >peak_gene_100k.bed
 bedtools intersect -a peak_gene_100k.bed -b common_data/RE_gene_corr_${genome}.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{if ($4==$9) print $1,$2,$3,$4,$5,$10}'|sed 's/\t/\_/1'|sed 's/\t/\_/1'>peak_gene_100k_corr.bed
